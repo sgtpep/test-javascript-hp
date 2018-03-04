5ef2686d8bc6fb5d92b8a79fe0dfd7186042df0d
Danil Semelenov <mail@danil.mobi>
2018-03-04 19:15:11 +0300

    Expand Component
    - handle data
    - add the component property to the element
    - support multiple class names

--- src/lib/component.js
+++ src/lib/component.js
@@ -1,16 +1,29 @@
 export default class Component {
-  constructor(name) {
+  constructor(name, data = null) {
     if (!name) throw 'No name argument';
     this.name = name;
-    this.element = createElement(this.render());
+    this.element = createElement(this, this.render());
     this.elements = queryElements(this.element, name);
+    if (this.data != null) this.data = data;
     if (this.style) appendStyle(this.style());
+    this.element.addEventListener(
+      'set-data',
+      event => (this.data = event.detail)
+    );
   }
 
   class(name = null, modifier = null) {
     return (
       this.name + (name ? `__${name}` : '') + (modifier ? `_${modifier}` : '')
     );
+  }
+
+  get data() {
+    throw 'get data() is not implemented';
+  }
+
+  set data(value) {
+    throw 'set data() is not implemented';
   }
 
   render() {
@@ -30,9 +43,10 @@ const appendStyle = css => {
   }
 };
 
-const createElement = html => {
+const createElement = (component, html) => {
   const element = document.createElement('div');
   element.innerHTML = html;
+  element.firstElementChild.component = component;
   return element.firstElementChild;
 };
 
@@ -40,14 +54,19 @@ const queryElements = (element, name) => {
   const elements = {};
   Array.from(element.querySelectorAll(`[class^=${name}__]`)).forEach(
     element => {
-      const name = element.className.split('__', 2)[1].split('_', 1)[0];
-      if (!elements[name]) {
-        elements[name] = element;
-      } else if (!Array.isArray(elements[name])) {
-        elements[name] = [elements[name], element];
-      } else {
-        elements[name].push(element);
-      }
+      Array.from(element.classList).forEach(className => {
+        const name = className
+          .split('__', 2)[1]
+          .split('_', 1)[0]
+          .replace(/-(.)/g, (match, char) => char.toUpperCase());
+        if (!elements[name]) {
+          elements[name] = element;
+        } else if (!Array.isArray(elements[name])) {
+          elements[name] = [elements[name], element];
+        } else {
+          elements[name].push(element);
+        }
+      });
     }
   );
   return elements;
